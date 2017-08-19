5eea8fcaca8df2444004977923f2f95c8bad23b9
Jovanny Cruz <jovannypcg@yahoo.com>
2017-08-19 11:17:06 -0500

    Add full url for a shorter link

--- app/helpers/short_links_helper.rb
+++ app/helpers/short_links_helper.rb
@@ -2,7 +2,12 @@ module ShortLinksHelper
   def slug_info(slug)
     content_tag(:section, class: "card") do
       content_tag(:h3, 'Your shorter link') +
-      content_tag(:p, slug)
+      content_tag(:a, full_url(slug), href: full_url(slug))
     end
   end
+
+  private
+    def full_url(slug)
+      "http://#{request.host_with_port}/#{slug}"
+    end
 end
