6d5aa2b619b0b99dd97548f8586cd81f3a037a84
Jovanny Cruz <jovannypcg@yahoo.com>
2017-08-20 12:05:01 -0500

    Add specs for ShortLink validations

--- spec/models/short_link_spec.rb
+++ spec/models/short_link_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe ShortLink, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    short_link = FactoryGirl.create(:short_link)
+    expect(short_link).to be_valid
+  end
+
+  context 'ShortLink has invalid data' do
+    let(:short_link_no_destination) { FactoryGirl.build(:short_link, destination: nil) }
+    let(:short_link_empty_destination) { FactoryGirl.build(:short_link, destination: '') }
+    let(:short_link_invalid_destination) { FactoryGirl.build(:short_link, destination: 'blahblah') }
+
+    it 'is not valid when no destination provided' do
+      expect(short_link_no_destination).to_not be_valid
+    end
+
+    it 'is not valid when empty string as destination is provided' do
+      expect(short_link_empty_destination).to_not be_valid
+    end
+
+    it 'is not valid when any string as destination is provided' do
+      expect(short_link_invalid_destination).to_not be_valid
+    end
+  end
 end
